969055fa4ad55fd84ba9ecfec3cfdc0b3f29f0ca
chiraghs <chiraghs1929@gmail.com>
2019-12-20 03:06:31 +0530

    Stack push,isEMpty, pop,topElement,Size

---
+++ Stacks_demo_push_pop/cmake-build-debug/CMakeFiles/Stacks_demo_push_pop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stacks_demo_push_pop.dir/main.cpp.o"
+  "Stacks_demo_push_pop"
+  "Stacks_demo_push_pop.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stacks_demo_push_pop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
