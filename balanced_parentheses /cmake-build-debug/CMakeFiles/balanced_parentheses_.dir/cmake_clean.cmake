1a5bb9ec9f38e07a538db0118ff0f0cbf9536f04
chiraghs <chiraghs1929@gmail.com>
2019-12-20 04:03:16 +0530

    Balanced Paranthesis

---
+++ balanced_parentheses /cmake-build-debug/CMakeFiles/balanced_parentheses_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/balanced_parentheses_.dir/main.cpp.o"
+  "balanced_parentheses_"
+  "balanced_parentheses_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/balanced_parentheses_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ balanced_parentheses /main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    string str;
+    cin>>str[10];
+    for (int i = 0; i <str.length() ; i++) {
+
+    }
+    return 0;
+}
