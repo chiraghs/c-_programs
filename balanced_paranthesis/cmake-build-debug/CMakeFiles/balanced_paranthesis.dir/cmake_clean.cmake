3914df9615ccc0a33928303427e43a8407569c4d
chiraghs <chiraghs1929@gmail.com>
2019-12-21 19:28:27 +0530

    balance paranthesis using stacks

---
+++ balanced_paranthesis/cmake-build-debug/CMakeFiles/balanced_paranthesis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/balanced_paranthesis.dir/main.cpp.o"
+  "balanced_paranthesis"
+  "balanced_paranthesis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/balanced_paranthesis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
